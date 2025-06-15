a7e08a0da30f6686fb8a7604529ebf10aa024063
rathin <rrachhadiya@hof-university.de>
2025-06-15 21:45:50 +0200

    Change routing Logic

--- pollify/src/main/java/com/rathin/pollify/controller/HomeController.java
+++ pollify/src/main/java/com/rathin/pollify/controller/HomeController.java
@@ -3,11 +3,18 @@ package com.rathin.pollify.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String login() {
-        return "redirect:/login.html";
+    public String login(HttpServletRequest request) {
+    HttpSession session = request.getSession(false);
+    if (session != null && session.getAttribute("username") != null) {
+        return "redirect:/home";
+    }
+    return "redirect:/login.html";
     }
 
     @RequestMapping("/register")
@@ -16,7 +23,12 @@ public class HomeController {
     }
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("username") == null) {
+            
+            return "redirect:/login.html";
+        }
         return "redirect:/user.html";
     }
 
