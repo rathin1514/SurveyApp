df277f9b2c05cb8d74adc11e2d8cf4d8615a1a7f
rathin <rrachhadiya@hof-university.de>
2025-06-15 18:26:52 +0200

    Basic static routing paths

--- pollify/src/main/java/com/rathin/pollify/HomeController.java
+++ pollify/src/main/java/com/rathin/pollify/HomeController.java
@@ -6,7 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
+    public String login() {
         return "redirect:/login.html";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "redirect:/register.html";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "redirect:/user.html";
+    }
+
+    @RequestMapping("/new")
+    public String createSurvey() {
+        return "redirect:/create.html";
+    }
+
+    @RequestMapping("/result")
+    public String showResults() {
+        return "redirect:/results.html";
+    }
+
+    @RequestMapping("/join")
+    public String startSurvey() {
+        return "redirect:/join.html";
     }
 }
