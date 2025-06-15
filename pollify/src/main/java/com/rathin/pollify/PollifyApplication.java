722906ceee6887b54343b768daaf2344ee5bf79e
rathin <rrachhadiya@hof-university.de>
2025-06-15 14:22:11 +0200

    Spring Boot Initialization

---
+++ pollify/src/main/java/com/rathin/pollify/PollifyApplication.java
@@ -0,0 +1,13 @@
+package com.rathin.pollify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollifyApplication.class, args);
+	}
+
+}
---
+++ pollify/src/test/java/com/rathin/pollify/PollifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rathin.pollify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
