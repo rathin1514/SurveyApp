6a220e6256ff4991d12cbfb156815ffb1977deef
rathin <rrachhadiya@hof-university.de>
2025-06-18 01:08:08 +0200

    Changed ENV variable for database link

--- database/init-db.sh
+++ database/init-db.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 if [ ! -f "$PGDATA/PG_VERSION" ]; then
-  initdb -D "$PGDATA"
+    initdb -D "$PGDATA"
+    echo "listen_addresses = '*'" >> "$PGDATA/postgresql.conf"
+    echo "host all all all md5" >> "$PGDATA/pg_hba.conf"
 
     pg_ctl -D "$PGDATA" -w start
 
-    psql -v ON_ERROR_STOP=1 --username=postgres <<-EOSQL
-        ALTER USER postgres PASSWORD 'password';
-        CREATE DATABASE pollify;
-    EOSQL
+    psql -v ON_ERROR_STOP=1 --username=postgres <<EOSQL
+ALTER USER postgres PASSWORD 'password';
+CREATE DATABASE pollify;
+EOSQL
 
+    pg_ctl -D "$PGDATA" -m fast -w stop
 fi
+
+exec postgres -D "$PGDATA"
