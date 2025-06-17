4f75692a1fde03869e4f65e6f94bcb1592160945
rathin <rrachhadiya@hof-university.de>
2025-06-17 23:47:14 +0200

    Postgre Dockerfile changes supporting successful build

--- database/init-db.sh
+++ database/init-db.sh
@@ -2,13 +2,12 @@
 
 if [ ! -f "$PGDATA/PG_VERSION" ]; then
   initdb -D "$PGDATA"
+
+    pg_ctl -D "$PGDATA" -w start
+
+    psql -v ON_ERROR_STOP=1 --username=postgres <<-EOSQL
+        ALTER USER postgres PASSWORD 'password';
+        CREATE DATABASE pollify;
+    EOSQL
+
 fi
-
-pg_ctl -D "$PGDATA" -w start
-
-psql -v ON_ERROR_STOP=1 --username=postgres <<-EOSQL
-  ALTER USER postgres PASSWORD 'password';
-  CREATE DATABASE pollify;
-EOSQL
-
-pg_ctl -D "$PGDATA" -m fast -w stop
